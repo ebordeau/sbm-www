9ca5591ed63fb4f01f8b07f53a5108f840711deb
Eric Bordeau <ebordeau@gmail.com>
2020-01-02 14:39:50 -0500

    Copy style.css over to the dist directory

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,5 +1,5 @@
 // Find the full example of all available configuration options at
 // https://github.com/muenzpraeger/create-lwc-app/blob/master/packages/lwc-services/example/lwc-services.config.js
 module.exports = {
-    resources: [{ from: 'src/images', to: 'dist/images' }],
+    resources: [{ from: 'src/images', to: 'dist/images' }, { from: 'src/style.css', to: 'dist/style.css' }],
 };
